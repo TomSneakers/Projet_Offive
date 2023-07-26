0968c9428ab7828634a1699d710d58107167890e
ninoherran <contact@ninoherran.fr>
2023-07-26 20:20:11 +0200

    fix: bootstrap import

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import FilterProvider from "./context/index.jsx";
 import {Provider} from "react-redux";
 import {store} from "./lib/redux/reducers/index.js";
