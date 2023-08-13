55e78891c4939fe4392a533efd78c58b71d3a72b
Tom <tomdesvignes031@gmail.com>
2023-08-13 22:14:08 +0200

    maj

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,37 +8,60 @@ function Product() {
   const location = useLocation();
   const product = location.state.product;
 
-  const [details, setDetails] = useState({ quantity: 1, size: "small" });
+  const [details, setDetails] = useState({ quantity: 1, size: "" });
+  const [price, setPrice] = useState(product.price);
 
   const dispatch = useDispatch();
 
   const addCart = () => {
+    if (details.size === "") {
+      alert("Veuillez sélectionner une taille.");
+      return;
+    }
     const item = {
       id: product.id,
       name: product.name,
       filter: product.filter,
-      price: product.price,
+      price: price,
       category: product.category,
     };
     dispatch(addToCart({ ...item, ...details }));
   };
-  const handleOnChange = (e) =>
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "size") {
+      const newSize = value;
+      let newPrice = product.price;
+      if (newSize === "medium") {
+        newPrice = product.price + 10;
+      } else if (newSize === "large") {
+        newPrice = product.price + 20;
+      }
+      setPrice(newPrice);
+    }
     setDetails((prevState) => ({
       ...prevState,
-      [e.target.name]: e.target.value,
+      [name]: value,
     }));
+  };
+
   return (
     <section className="pt-5 pb-5">
       <div className="container">
         <div className="row">
           <div className="col-md-6 text-center">
             <div className="product-image d-block mt-3">
-              <img className="img-fluid" src={`images/${product.id}.png`} />
+              <img
+                className="img-fluid"
+                src={`images/${product.id}.png`}
+                alt={product.name}
+              />
             </div>
           </div>
           <div className="col-md-6 mt-5 mt-md-2 text-center text-md-left">
             <h2 className="mb-3 mt-0">{product.name}</h2>
-            <p className="lead mt-2 mb-3 primary-color">{product.price}€</p>
+            <p className="lead mt-2 mb-3 primary-color">{price}€</p>
             <h5 className="mt-4">Description</h5>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
@@ -60,15 +83,18 @@ function Product() {
               <div className="col-6">
                 <label htmlFor="size">Size</label>
                 <select
-                  //   defaultValue="small"
+                  defaultValue=""
                   name="size"
                   id="size"
-                  className="custom-select form-control  mb-4"
+                  className="custom-select form-control mb-4"
                   onChange={handleOnChange}
                 >
-                  <option value="small">Small</option>
-                  <option value="medium">Medium</option>
-                  <option value="large">Large</option>
+                  <option value="" disabled>
+                    Sélectionnez votre taille
+                  </option>
+                  <option value="small">Petite</option>
+                  <option value="medium">Normale</option>
+                  <option value="large">Grande</option>
                 </select>
               </div>
               <div className="col-6">
@@ -87,14 +113,15 @@ function Product() {
               type="button"
               className="btn btn-block btn-lg border border-orange text-orange"
               onClick={addCart}
+              disabled={details.size === ""}
             >
-              Add to cart
+              Ajouter au panier
             </button>
           </div>
         </div>
         <div className="col-sm-6 mb-3 mb-m-1 order-md-1 text-md-left">
           <Link to="/">
-            <i className="fas fa-arrow-left mr-2"></i> Back
+            <i className="fas fa-arrow-left mr-2"></i> Retour
           </Link>
         </div>
       </div>
