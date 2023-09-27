61efb98708619756b14a9c1df20e0e7931be76e6
Tom <tomdesvignes031@gmail.com>
2023-09-27 13:09:39 +0200

    maj lib

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -1,11 +1,11 @@
 // Définition de la requête rest GET_ORDERS
 export async function GET_ORDERS() {
-  const result = await fetch("http://localhost:2023/orders", { method: "get" });
+  const result = await fetch("http://ninoherran.fr:2023/orders", { method: "get" });
   return result.json();
 }
 
 export async function AddOrder(order) {
-  const result = await fetch("http://localhost:2023/orders", {
+  const result = await fetch("http://ninoherran.fr:2023/orders", {
     method: "post",
     body: JSON.stringify({ ...order }),
     headers: {
@@ -24,12 +24,12 @@ export function getFromCache(name) {
 }
 
 async function getAllProducts() {
-  const result = await fetch("http://localhost:2023/products", { method: "get" });
+  const result = await fetch("http://ninoherran.fr:2023/products", { method: "get" });
   return result.json();
 }
 
 async function getProducts(category) {
-  const result = await fetch(`http://localhost:2023/products/${category}`, { method: "get" });
+  const result = await fetch(`http://ninoherran.fr:2023/products/${category}`, { method: "get" });
   return result.json();
 }
 
